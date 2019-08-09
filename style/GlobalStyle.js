ee29a662b94cfcea20f525a9f9f17baebe80049a
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-08 22:30:33 -0700

    Fot fix

--- style/GlobalStyle.js
+++ style/GlobalStyle.js
@@ -1,38 +1,8 @@
 import { createGlobalStyle, css } from 'styled-components'
 import { reset } from 'styled-reset'
 import { mq } from './theme'
-import AmericaLtWoff2 from '../assets/fonts/GT-America-Standard-Light.woff2'
-import AmericaLtWoff from '../assets/fonts/GT-America-Standard-Light.woff'
-import AmericaLtTtf from '../assets/fonts/GT-America-Standard-Light.ttf'
-
-import AmericaMdWoff2 from '../assets/fonts/GT-America-Standard-Medium.woff2'
-import AmericaMdWoff from '../assets/fonts/GT-America-Standard-Medium.woff'
-import AmericaMdTtf from '../assets/fonts/GT-America-Standard-Medium.ttf'
-
-import LyonLtWoff from '../assets/fonts/LyonDisplay-Light-Web.woff'
-import LyonLtWoff2 from '../assets/fonts/LyonDisplay-Light-Web.woff2'
 
 const style = css`
-  @font-face {
-    font-family: 'America';
-    src: url(${AmericaLtWoff2}) format('woff2'),
-      url(${AmericaLtWoff}) format('woff'), url(${AmericaLtTtf}) format('ttf');
-    font-weight: 300;
-  }
-
-  @font-face {
-    font-family: 'America';
-    src: url(${AmericaMdWoff2}) format('woff2'),
-      url(${AmericaMdWoff}) format('woff'), url(${AmericaMdTtf}) format('ttf');
-    font-weight: 500;
-  }
-
-  @font-face {
-    font-family: 'Lyon';
-    src: url(${LyonLtWoff2}) format('woff2'), url(${LyonLtWoff}) format('woff');
-    font-weight: 500;
-  }
-
   * {
     box-sizing: border-box;
   }
