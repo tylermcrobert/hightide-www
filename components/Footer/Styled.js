838f6ea7b5630adc1aa1bf42830c9d703b3b75fd
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-09 18:33:01 -0700

    Footer

--- components/Footer/Styled.js
+++ components/Footer/Styled.js
@@ -1,15 +1,56 @@
 import styled from 'styled-components'
+import { mq } from '../../style/theme'
 
 const Footer = styled.div`
   background: ${props => props.theme.colors.primary};
   padding: 1px 0;
   color: ${props => props.theme.colors.secondary};
-  font-size: ${props => props.theme.fontSizes[2]};
+  font-size: ${props => props.theme.fontSizes[1]};
   line-height: 1.5;
 
-  p {
+  p,
+  a {
     color: ${props => props.theme.colors.grey};
+    display: block;
   }
 `
 
-export default { Footer }
+const Grid = styled.div`
+  @media ${mq.xs} {
+    display: grid;
+    grid-gap: ${props => props.theme.margins.standard};
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media ${mq.md} {
+    grid-template-columns: 2fr 1fr 1fr;
+  }
+`
+
+const StayUpdated = styled.input`
+  margin: 0;
+  border: 0;
+  padding: 0;
+  display: block;
+  white-space: normal;
+  background: none;
+  font-size: inherit;
+  color: ${props => props.theme.colors.grey};
+  width: 100%;
+  max-width: 14em;
+
+  margin-top: ${props => props.theme.margins.standard};
+
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid ${props => props.theme.colors.grey};
+
+  transition: 400ms border-bottom-color ${props => props.theme.ease.standard};
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid ${props => props.theme.colors.secondary};
+    color: ${props => props.theme.colors.secondary};
+  }
+`
+
+export default { Footer, Grid, StayUpdated }
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,21 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { validate } from 'email-validator'
 import Styled from './Styled'
 import { Wrap, Section } from '../../style'
 
 function Footer() {
+  const [email, setEmail] = useState('')
+  const [valid, setValid] = useState(false)
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+
+  function handleChange(e) {
+    const { value } = e.target
+    setEmail(value)
+    setValid(validate(value))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (valid) {
+      setError(null)
+      setSuccess(': )')
+      console.log(`${email} is valid`)
+    } else {
+      setError(': (')
+      setSuccess(null)
+    }
+
+    console.log(valid)
+  }
   return (
     <Styled.Footer>
-      <Section>
-        <Wrap>
-          <h5>Visit</h5>
-          <p>
-            231 Front Street, Ste 209
-            <br />
-            Brooklyn, New York 11201
-          </p>
-          <p>© 2018 High Tide Creative</p>
-        </Wrap>
-      </Section>
+      <Wrap>
+        <Styled.Grid>
+          <Section>
+            <h5>Visit</h5>
+            <p>
+              231 Front Street, Ste 209
+              <br />
+              Brooklyn, New York 11201
+            </p>
+            <p>© 2018 High Tide Creative</p>
+          </Section>
+          <Section>
+            <h5>Inquiries</h5>
+            <a href="mailto:info@hightidenyc.com">info@hightidenyc.com</a>
+          </Section>
+          <Section>
+            <h5>Follow</h5>
+            <a href="">Instagram</a>
+            <a href="">Facebook</a>
+            <a href="">Twitter</a>
+            <a href="">Tumblr</a>
+            <form onSubmit={handleSubmit}>
+              <Styled.StayUpdated
+                onChange={handleChange}
+                placeholder="Stay Updated"
+              />
+            </form>
+            {error && error}
+            {success && success}
+          </Section>
+        </Styled.Grid>
+      </Wrap>
     </Styled.Footer>
   )
 }
