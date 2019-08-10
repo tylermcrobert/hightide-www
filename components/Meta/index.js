f8d6ce8fddb9372741110078f5c2fbc65b0a74c2
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-10 11:57:10 -0700

    Add favicon

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 import config, { siteUrl, social, previewImage } from '../../site.config'
-
 import formatTitle from '../../util/formatTitle'
 
 const Meta = ({ url, title: titleProp, image, description }) => (
   <Head>
     <meta charset="utf-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+    <Favicon />
 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content={description} />
@@ -29,6 +30,38 @@ const Meta = ({ url, title: titleProp, image, description }) => (
   </Head>
 )
 
+function Favicon() {
+  const path = '/static/favicon'
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`${path}/apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={`${path}/favicon-32x32.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`${path}/favicon-16x16.png`}
+      />
+      <link rel="manifest" href={`${path}/site.webmanifest`} />
+      <link
+        rel="mask-icon"
+        href={`${path}/safari-pinned-tab.svg`}
+        color="#000000"
+      />
+      <meta name="msapplication-TileColor" content="#ff0000" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  )
+}
 Meta.defaultProps = {
   title: null,
   image: previewImage,
