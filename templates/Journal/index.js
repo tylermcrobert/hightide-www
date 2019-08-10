744370d6680f89f7ff8b43cfccb10073bd723227
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-10 14:22:54 -0700

    Put section around Journal

--- templates/Journal/index.js
+++ templates/Journal/index.js
@@ -2,32 +2,34 @@ import React from 'react'
 // import { RichText } from 'prismic-reactjs'
 import Link from 'next/link'
 import { RichText } from 'prismic-reactjs'
-import { Wrap } from '../../style'
+import { Wrap, Section } from '../../style'
 import Styled from './Styled'
 
 export default function Journal({ data }) {
   return (
-    <Wrap>
-      <Styled.Wrapper>
-        {data.results.map(item => {
-          const publishDate = item.data.date
-          const formattedDate = publishDate.replace(/-/g, '.')
-          const title = RichText.asText(item.data.title)
-          const mainImg = item.data.main_image.url
-          const thumbnail = item.data.thumbnail_image.url
+    <Section>
+      <Wrap>
+        <Styled.Wrapper>
+          {data.results.map(item => {
+            const publishDate = item.data.date
+            const formattedDate = publishDate.replace(/-/g, '.')
+            const title = RichText.asText(item.data.title)
+            const mainImg = item.data.main_image.url
+            const thumbnail = item.data.thumbnail_image.url
 
-          return (
-            <JournalCard
-              key={item.uid}
-              uid={item.uid}
-              image={thumbnail || mainImg}
-              date={formattedDate}
-              title={title}
-            />
-          )
-        })}
-      </Styled.Wrapper>
-    </Wrap>
+            return (
+              <JournalCard
+                key={item.uid}
+                uid={item.uid}
+                image={thumbnail || mainImg}
+                date={formattedDate}
+                title={title}
+              />
+            )
+          })}
+        </Styled.Wrapper>
+      </Wrap>
+    </Section>
   )
 }
 
