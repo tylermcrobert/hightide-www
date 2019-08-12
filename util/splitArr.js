df2258152afe5ec2505fa340e951665fbd1d01cc
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-08-12 15:19:53 -0700

    Update splitArr.js

--- util/splitArr.js
+++ util/splitArr.js
@@ -1,30 +1,7 @@
-/*
-  Get every nth item item [nthItem]
-  starting at y [start]
-*/
-const nth = (arr, nthItem, start = 0) =>
-  arr
-    /*
-      get starting point by making a new
-      array without first [start]
-    */
-    .slice(start, arr.length)
-    /*
-      use new shortened array
-      to grab every x number
-    */
-    .filter((_, i) => i % nthItem === 0);
 
-/* split array into subarrays */
-const splitArr = (arr, cols) =>
-  /*
-    create empty array (col amount) items
-  */
-  Array.from({ length: cols })
-    /*
-      loop through and get every (col amount)
-      items, starting at (col number)
-    */
-    .map((item, i) => nth(arr, cols, i));
-
-export default splitArr;
+const splitArray = (inputArray, subArrays) =>
+  Array.from({ length: subArrays }).map((_, subArrIndex) =>
+    inputArray
+      .slice(subArrIndex, inputArray.length)
+      .filter((filterItem, filterIndex) => filterIndex % subArrays === 0)
+  );
