c42374c5e8f0b5eb275d2165c3eea9dc1d3abac5
Tyler <tyler.mcrobert@gmail.com>
2019-08-15 23:12:49 -0700

    Grid template mobile

--- templates/CaseStudy/Gallery/Styled.js
+++ templates/CaseStudy/Gallery/Styled.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { mq } from '../../../style/theme'
 
 const GalleryWrap = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap ${props => props.theme.margins.standard};
+    display: grid;
+    grid-gap ${props => props.theme.margins.standard};
+
+    @media ${mq.sm}{
+      grid-template-columns: 1fr 1fr;
+    }
 `
 
 export default { GalleryWrap }
