acdfe518264cce80294253b9ec7e7c0a6955585c
tylermcrobert <tyler.mcrobert@gmail.com>
2019-09-24 23:39:47 -0700

    Tightened <Section /> margins

--- components/Section/index.js
+++ components/Section/index.js
@@ -17,20 +17,18 @@ const FadeIn = ({ ...props }) => {
 }
 
 const Section = styled(FadeIn)`
-  margin: 4rem 0;
+  margin: 3rem 0;
 
   @media ${mq.sm} {
-    margin: 5rem 0;
+    margin: 4rem 0;
   }
   @media ${mq.md} {
-    margin: 6rem 0;
+    margin: 4.5rem 0;
   }
   @media ${mq.lg} {
-    margin: 6rem 0;
+    margin: 5rem 0;
   }
-  @media ${mq.xl} {
-    margin: 8rem 0;
-  }
+
   ${props =>
     props.noTop &&
     css`
@@ -41,17 +39,6 @@ const Section = styled(FadeIn)`
     css`
       margin-bottom: 0 !important;
     `}
-`
-
-Section.Large = styled(Section)`
-  margin: 8rem 0;
-
-  @media ${mq.md} {
-    margin: 10rem 0;
-  }
-  @media ${mq.lg} {
-    margin: 12rem 0;
-  }
 `
 
 export default Section
