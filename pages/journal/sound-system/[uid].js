737a0893ed5b8f78c048b16df2ab3572d283cacb
tylermcrobert <tyler.mcrobert@gmail.com>
2019-09-28 13:17:14 -0700

    Preview-proof sound system page

--- pages/journal/sound-system/[uid].js
+++ pages/journal/sound-system/[uid].js
@@ -2,41 +2,50 @@ import { Client } from 'util/prismic'
 import Meta from 'components/Meta'
 import { RichText } from 'prismic-reactjs'
 import SoundSystemTemplate from 'templates/SoundSystem'
+import Error from 'next/error'
 
 export default function SoundSystem({ spotifyData, spotifyRes, prismicRes }) {
-  const tracks = spotifyRes.tracks.items
-  const image = prismicRes.data.image.url || spotifyRes.images[0].url
+  if (prismicRes && spotifyRes) {
+    const tracks = spotifyRes.tracks.items
+    const image = prismicRes.data.image.url || spotifyRes.images[0].url
 
-  return (
-    <>
-      <Meta
-        title={RichText.asText(prismicRes.data.title)}
-        image={image}
-        url={`journal/sound-system/${prismicRes.uid}`}
-      />
-      <SoundSystemTemplate
-        tracks={tracks}
-        data={prismicRes.data}
-        image={image}
-        spotifyData={spotifyData}
-      />
-    </>
-  )
+    return (
+      <>
+        <Meta
+          title={RichText.asText(prismicRes.data.title)}
+          image={image}
+          url={`journal/sound-system/${prismicRes.uid}`}
+        />
+        <SoundSystemTemplate
+          tracks={tracks}
+          data={prismicRes.data}
+          image={image}
+          spotifyData={spotifyData}
+        />
+      </>
+    )
+  }
+  return <Error statusCode={404} />
 }
 
 SoundSystem.getInitialProps = async ({ req, query }) => {
-  const { uid } = query
-  const soundSystem = await Client(req).getByUID('sound_system', uid)
-  const spotifyData = SoundSystem.parsePrismicSpotifyData(soundSystem)
+  try {
+    const { uid } = query
+    const soundSystem = await Client(req).getByUID('sound_system', uid)
+    const spotifyData = SoundSystem.parsePrismicSpotifyData(soundSystem)
 
-  const API_ROUTE =
-    'https://tm-hightide.netlify.com/.netlify/functions/getSpotifyAuth'
+    const API_ROUTE =
+      'https://tm-hightide.netlify.com/.netlify/functions/getSpotifyAuth'
 
-  const spotifyRes = await fetch(`${API_ROUTE}?id=${spotifyData.id}`)
-    .then(res => res.json())
-    .then(data => data.body)
+    const spotifyRes = await fetch(`${API_ROUTE}?id=${spotifyData.id}`)
+      .then(res => res.json())
+      .then(data => data.body)
 
-  return { prismicRes: soundSystem, spotifyData, spotifyRes }
+    return { prismicRes: soundSystem, spotifyData, spotifyRes }
+  } catch (err) {
+    console.error(err)
+    return {}
+  }
 }
 
 SoundSystem.parsePrismicSpotifyData = res => {
