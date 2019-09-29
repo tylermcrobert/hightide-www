a1eb1628919138cf18ff8e305d6d5d4ad71e29e7
tylermcrobert <tyler.mcrobert@gmail.com>
2019-09-29 12:49:53 -0700

    Catch preview error

--- pages/preview.js
+++ pages/preview.js
@@ -5,15 +5,19 @@ import { Client, linkResolver } from 'middleware/prismic'
 const Preview = () => <div>Preview</div>
 
 Preview.getInitialProps = async context => {
-  const { token } = context.query
-  const { res, req } = context
+  try {
+    const { token } = context.query
+    const { res, req } = context
 
-  const url = await Client(req).previewSession(token, linkResolver, '/')
-  if (res) {
-    res.writeHead(302, { Location: url })
-    res.end()
-  } else {
-    Router.push(url)
+    const url = await Client(req).previewSession(token, linkResolver, '/')
+    if (res) {
+      res.writeHead(302, { Location: url })
+      res.end()
+    } else {
+      Router.push(url)
+    }
+  } catch (e) {
+    console.error(e)
   }
   return {}
 }
