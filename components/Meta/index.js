d0eb526bcfe439af6fa8dd143e8f8ab2f2628f3e
tylermcrobert <tyler.mcrobert@gmail.com>
2019-09-29 10:31:20 -0700

    Update favicon

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -39,38 +39,41 @@ const Meta = ({ url, title: titleProp, image, description }) => {
   )
 }
 
-function Favicon() {
-  const path = '/static/favicon'
-  return (
-    <>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href={`${path}/apple-touch-icon.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${path}/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${path}/favicon-16x16.png`}
-      />
-      <link rel="manifest" href={`${path}/site.webmanifest`} />
-      <link
-        rel="mask-icon"
-        href={`${path}/safari-pinned-tab.svg`}
-        color="#000000"
-      />
-      <meta name="msapplication-TileColor" content="#ff0000" />
-      <meta name="theme-color" content="#ffffff" />
-    </>
-  )
-}
+const Favicon = () => (
+  <>
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/static/favicon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/static/favicon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/static/favicon/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/static/favicon/site.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="/static/favicon/safari-pinned-tab.svg"
+      color="#000000"
+    />
+    <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta
+      name="msapplication-config"
+      content="/static/favicon/browserconfig.xml"
+    />
+    <meta name="theme-color" content="#ffffff" />
+  </>
+)
+
 Meta.defaultProps = {
   title: null,
   image: previewImage,
