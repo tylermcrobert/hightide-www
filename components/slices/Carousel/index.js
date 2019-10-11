304ca3b53b352b28fd30e340e04e5feaa4e269d5
Tyler McRobert <tyler.mcrobert@gmail.com>
2019-10-10 18:33:29 -0700

    Use carousel globally

--- components/slices/Carousel/index.js
+++ components/slices/Carousel/index.js
@@ -1,74 +1,35 @@
-import React, { useContext, useState, createContext } from 'react'
+import React from 'react'
 import Section from 'components/Section'
 import PropTypes from 'prop-types'
-import Fpo from 'components/Fpo'
-import Styled from './Styled'
+import { Wrapper as CarouselWrap, Nav, ImageWrapper } from 'components/Carousel'
+import AspectImage from 'components/AspectImage'
 
-const CarouselCtx = createContext()
 function Carousel({ data }) {
-  const aspects = data.items.map(item => {
-    const dims = item.carousel_image.dimensions
-    return dims.height / dims.width
-  })
+  if (data) {
+    const aspects = data.items.map(item => {
+      const dims = item.carousel_image.dimensions
+      return dims.height / dims.width
+    })
 
-  const smallestAspect = Math.min(...aspects)
+    const smallestAspect = Math.min(...aspects)
 
-  const urls = data.items.map(item => item.carousel_image.url)
-  return (
-    <Section>
-      <Fpo>
-        <Layout urls={urls} smallestAspect={smallestAspect} />
-      </Fpo>
-    </Section>
-  )
+    const images = data.items.map(item => (
+      <AspectImage src={item.carousel_image.url} aspect={smallestAspect} />
+    ))
+    return (
+      <Section>
+        <CarouselWrap items={images}>
+          <ImageWrapper />
+          <Nav />
+        </CarouselWrap>
+      </Section>
+    )
+  }
+  return null
 }
 
-const Layout = ({ urls, smallestAspect }) => {
-  const [index, setCurrentIndex] = useState(0)
-
-  const { length } = urls
-  const nextIndex = (index + 1) % urls.length
-  const prevIndex = (index + length - 1) % length
-
-  const getNext = () => setCurrentIndex(nextIndex)
-  const getPrev = () => setCurrentIndex(prevIndex)
-
-  return (
-    <CarouselCtx.Provider value={{ length, index, getNext, getPrev }}>
-      {urls.map((url, i) => (
-        <Styled.Image
-          src={url}
-          key={url}
-          active={i === index}
-          aspect={smallestAspect}
-        />
-      ))}
-
-      <Navigation />
-    </CarouselCtx.Provider>
-  )
+Carousel.propTypes = {
+  data: PropTypes.object.isRequired,
 }
-
-Layout.propTypes = {
-  urls: PropTypes.array.isRequired,
-  smallestAspect: PropTypes.number.isRequired,
-}
-
-/**
- * Navigation
- */
-
-const Navigation = () => {
-  const { getNext, getPrev, length, index } = useContext(CarouselCtx)
-  return (
-    <Styled.FpoNav>
-      0{index + 1} of 0{length}
-      <div onClick={getPrev}>Prev</div>
-      <div onClick={getNext}>Next</div>
-    </Styled.FpoNav>
-  )
-}
-
-// Carousel.propTypes = {}
 
 export default Carousel
