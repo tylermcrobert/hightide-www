bf972da2dccfe05f3099e87f0ba888793991d7a4
tylermcrobert <tyler.mcrobert@gmail.com>
2019-10-11 19:18:09 -0700

    Cleaned up

--- components/Carousel/ImageWrapper/Styled.js
+++ components/Carousel/ImageWrapper/Styled.js
@@ -4,19 +4,9 @@ const GalleryWrapper = styled.div`
   overflow: hidden;
 `
 
-const translateVal = ({ theme }) => {
-  if (theme.restarting) {
-    return 100 / (theme.count + 1) - 100
-  }
-
-  return -(100 / (theme.count + 1)) * theme.index
-}
-
 const SlideWrapper = styled.ul`
   display: flex;
   width: ${({ theme }) => (theme.count + 1) * 100}%;
-  transform: translate3d(${translateVal}%, 0, 0);
-  transition: transform 500ms ${props => props.theme.ease.standard};
 `
 
 const Slide = styled.div`
--- components/Carousel/ImageWrapper/index.js
+++ components/Carousel/ImageWrapper/index.js
@@ -1,37 +1,43 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import PropTypes from 'prop-types'
-import { ThemeProvider } from 'styled-components'
 import { CarouselCtx } from '..'
 import usePrevious from './hooks/usePrevious'
 import Styled from './Styled'
 
 const ImageWrapper = () => {
   const { items, index: currentIndex } = useContext(CarouselCtx)
+  const slideWrapperRef = useRef()
 
   const prevIndex = usePrevious(currentIndex)
-  const restarting = !!(prevIndex && currentIndex === 0)
+  const isRestarting = !!(prevIndex && currentIndex === 0)
 
   if (items && items.length) {
     return (
-      <ThemeProvider
-        theme={{ count: items.length, index: currentIndex, restarting }}
-      >
-        <Styled.GalleryWrapper>
-          <Styled.SlideWrapper as="ul" count={items.length}>
-            {[...items, items[0]].map((item, i) => (
-              <Slide
-                current={i === currentIndex}
-                key={(item && `${item.key} ${i}`) || i}
-              >
-                {item}
-              </Slide>
-            ))}
-          </Styled.SlideWrapper>
-        </Styled.GalleryWrapper>
-      </ThemeProvider>
+      <Styled.GalleryWrapper>
+        <Styled.SlideWrapper ref={slideWrapperRef}>
+          <Slides />
+        </Styled.SlideWrapper>
+      </Styled.GalleryWrapper>
     )
   }
   return null
+}
+
+const Slides = () => {
+  const { items, index: currentIndex } = useContext(CarouselCtx)
+  /* add first item to end */
+  const infiniteItems = [...items, items[0]]
+
+  return infiniteItems.map((item, i) => {
+    const isCurrent = i === currentIndex
+    const key = (item && `${item.key} ${i}`) || i
+
+    return (
+      <Slide current={isCurrent} key={key}>
+        {item}
+      </Slide>
+    )
+  })
 }
 
 const Slide = ({ children, current }) => {
