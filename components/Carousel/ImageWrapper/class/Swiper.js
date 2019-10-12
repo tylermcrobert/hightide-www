881427fa4706b5ae501e998571e24791bbb2b6e5
tylermcrobert <tyler.mcrobert@gmail.com>
2019-10-11 22:05:55 -0700

    Make private methods in class

--- components/Carousel/ImageWrapper/class/Swiper.js
+++ components/Carousel/ImageWrapper/class/Swiper.js
@@ -4,45 +4,49 @@ export default class Swiper {
     this.distance = 0
   }
 
-  register(el) {
-    this.el = el
-    this.setWidth()
-    this.addListners()
-  }
-
-  setTranslateVal() {
+  _setTranslateVal() {
     const dist = this.el.style.transform.split('(')[1].split('px')[0]
     const parsedDist = parseInt(dist, 0)
     this.distance = parsedDist
   }
 
-  swipe = delta => {
-    const adv = this.distance + delta
-    this.setX(adv)
-  }
-
-  setSlide(dist) {
-    this.setX(dist)
-    this.setTranslateVal()
-  }
-
-  setX = dist => {
+  _setX = dist => {
     this.el.style.transform = `translateX(${dist}px)`
   }
 
-  addListners = () => {
-    window.addEventListener('resize', this.handleResize)
-  }
-
-  removeListeners = () => {
-    window.removeEventListener('resize', this.handleResize)
-  }
-
-  setWidth = () => {
+  _setWidth = () => {
     this.width = this.el.offsetWidth
   }
 
-  handleResize = () => {
-    this.setWidth()
+  _addListners = () => {
+    window.addEventListener('resize', this._handleResize)
+  }
+
+  _handleResize = () => {
+    this._setWidth()
+  }
+
+  /*
+   *  Public Methods
+   */
+
+  register(el) {
+    this.el = el
+    this._setWidth()
+    this._addListners()
+  }
+
+  swipe = delta => {
+    const adv = this.distance + delta
+    this._setX(adv)
+  }
+
+  setSlide(dist) {
+    this._setX(dist)
+    this._setTranslateVal()
+  }
+
+  removeListeners = () => {
+    window.removeEventListener('resize', this._handleResize)
   }
 }
