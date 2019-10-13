f6c880840956e8541cc5c706d8835b3c8949db55
tylermcrobert <tyler.mcrobert@gmail.com>
2019-10-13 13:31:42 -0700

    Update marquee logic

--- components/Marquee/Styled.js
+++ components/Marquee/Styled.js
@@ -1,5 +1,4 @@
 import styled, { keyframes } from 'styled-components'
-import { mq } from 'style/theme'
 
 const Wrapper = styled.div`
   min-width: 100%;
@@ -10,24 +9,20 @@ const slide = keyframes`
   from {
     transform: translate3d(0,0,0);
   }
+
   to {
     transform: translate3d(-50%, 0, 0);
   }
 `
 
-const speed = factor => props => props.speed * factor || 10
-
 const Track = styled.div`
-  width: ${props => props.width}px;
   min-width: 200%;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  animation-play-state: ${props => (props.paused ? 'paused' : 'running')};
+  animation: ${slide} 10s infinite linear;
 
-  animation: ${slide} ${speed(0.8)}s infinite linear;
-
-  @media ${mq.md} {
-    animation: ${slide} ${speed(1)}s infinite linear;
+  &:hover {
+    animation-play-state: paused;
   }
 `
 export default { Wrapper, Track }
