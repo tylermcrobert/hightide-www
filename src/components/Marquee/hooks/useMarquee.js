65804f8c00bc857f12bc572e407148bff8a41370
tylermcrobert <tyler.mcrobert@gmail.com>
2019-10-15 18:20:27 -0700

    Refactor marquee and add pause toggle

--- src/components/Marquee/hooks/useMarquee.js
+++ src/components/Marquee/hooks/useMarquee.js
@@ -1,19 +1,20 @@
-import { useRef, useEffect, useCallback } from 'react'
+import { useRef, useEffect, createRef } from 'react'
 import { TweenMax } from 'gsap'
 
 const DEFAULT = 250
+const container = createRef()
+const wrapper = createRef()
+const inner = createRef()
 
-export default function useMarquee(speed) {
-  const container = useRef()
-  const wrapper = useRef()
+const animSpeed = createRef()
+const tween = createRef()
+
+/**
+ * Create functions for updating DOM
+ */
+
+function useDOMFunctions() {
   const containerWidth = useRef()
-  const inner = useRef()
-  const tween = useRef()
-
-  const getDuration = useCallback(
-    () => containerWidth.current / (DEFAULT * speed),
-    [speed]
-  )
 
   const setWrapWidth = () => {
     wrapper.current.style.width = ''
@@ -21,38 +22,83 @@ export default function useMarquee(speed) {
     wrapper.current.style.width = `${containerWidth.current}px`
   }
 
+  const getDuration = () =>
+    containerWidth.current / (DEFAULT * animSpeed.current)
+
+  return { setWrapWidth, getDuration }
+}
+
+/**
+ * Handle resizing
+ */
+
+function useResize() {
+  const { setWrapWidth, getDuration } = useDOMFunctions()
+
   const handleResize = () => {
     setWrapWidth()
     const dur = getDuration()
     tween.current.duration(dur)
   }
 
-  const animate = useCallback(() => {
-    const duration = getDuration()
-
-    tween.current = TweenMax.to(wrapper.current, duration, {
-      x: '-50%',
-      /* eslint-disable no-undef */
-      ease: Linear.easeNone,
-      repeat: -1,
-    })
-  }, [getDuration])
-
-  // setup on mount
-  useEffect(() => {
-    wrapper.current = container.current.querySelector('.mq-wrapper')
-    inner.current = container.current.querySelector('.mq-inner')
-    setWrapWidth()
-    animate()
-  }, [animate])
-
-  // setup and clean up listeners
   useEffect(() => {
     window.addEventListener('resize', handleResize)
     return () => {
       window.removeEventListener('resize', handleResize)
     }
   })
+}
+
+/**
+ * UsePause
+ */
+
+function usePauseToggle() {
+  useEffect(() => {
+    const pauseTween = () => tween.current.pause()
+    const resumeTween = () => tween.current.resume()
+
+    container.current.addEventListener('mouseover', pauseTween)
+    container.current.addEventListener('mouseleave', resumeTween)
+
+    return () => {
+      container.current.removeEventListener('mouseover', pauseTween)
+      container.current.removeEventListener('mouseleave', resumeTween)
+    }
+  })
+}
+
+/**
+ * This is where the magic happens
+ */
+
+function useAnimation() {
+  const { setWrapWidth, getDuration } = useDOMFunctions()
+
+  useEffect(() => {
+    setWrapWidth()
+    const duration = getDuration()
+    tween.current = TweenMax.to(wrapper.current, duration, {
+      x: '-50%',
+      /* eslint-disable no-undef */
+      ease: Linear.easeNone,
+      repeat: -1,
+    })
+  }, [getDuration, setWrapWidth])
+}
+
+export default function useMarquee(speed) {
+  animSpeed.current = speed
+
+  // register DOM
+  useEffect(() => {
+    wrapper.current = container.current.querySelector('.mq-wrapper')
+    inner.current = container.current.querySelector('.mq-inner')
+  }, [])
+
+  useAnimation()
+  useResize()
+  usePauseToggle()
 
   return { ref: container }
 }
