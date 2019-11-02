02a1dc4157bb6ccaa376e9aeb472d5876e20ee45
Tyler McRobert <tyler.mcrobert@gmail.com>
2019-11-01 23:35:00 -0700

    Add product page

--- src/templates/Product/index.js
+++ src/templates/Product/index.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Wrap } from 'style'
 import Section from 'components/Section'
+import Styled from './Styled'
 // import PropTypes from 'prop-types'
 
 const Product = ({ data }) => {
   const { descriptionHtml, title } = data
 
-  console.log(data)
-
   return (
     <Wrap>
       <Section>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
+        <Styled.Wrapper>
+          <img src={data.images[0].src} alt={title} />
+          <div>
+            <h1>{title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
+          </div>
+        </Styled.Wrapper>
       </Section>
     </Wrap>
   )
