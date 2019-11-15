77e57152878e6dc5d21579a45e5725e531f27766
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-11-14 22:11:33 -0800

    Add <Link/> to products page

--- src/templates/Store/index.js
+++ src/templates/Store/index.js
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react'
 import { Wrap } from 'style'
 import AspectImage from 'components/AspectImage'
+import Link from 'next/link'
 import Styled from './Styled'
 
 export const StoreContext = createContext()
@@ -19,18 +20,20 @@ function Store({ products }) {
 
 const Product = ({ product }) => {
   return (
-    <a href={`/store/${product.handle}/`}>
-      <div key={product.id}>
-        {product.images[0] && (
-          <AspectImage src={product.images[0].src} alt={product.title} />
-        )}
+    <Link href="/store/[handle]" as={`/store/${product.handle}/`}>
+      <a>
+        <div key={product.id}>
+          {product.images[0] && (
+            <AspectImage src={product.images[0].src} alt={product.title} />
+          )}
 
-        <Styled.ItemTitle>
-          <h2>{product.title}</h2>
-          <h5>${product.variants[0].price}</h5>
-        </Styled.ItemTitle>
-      </div>
-    </a>
+          <Styled.ItemTitle>
+            <h2>{product.title}</h2>
+            <h5>${product.variants[0].price}</h5>
+          </Styled.ItemTitle>
+        </div>
+      </a>
+    </Link>
   )
 }
 
