734fc2bf846e0cb8de21fe260b892197ab041be6
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-11-17 14:56:11 -0800

    remove noTop

--- src/templates/Product/index.js
+++ src/templates/Product/index.js
@@ -26,7 +26,7 @@ const Product = memo(({ data: productData }) => {
     >
       <Wrap>
         <Styled.Wrapper>
-          <Section noTop>
+          <Section>
             <ProductImages />
           </Section>
           <Section>
