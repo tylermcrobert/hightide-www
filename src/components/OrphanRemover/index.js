fc83dce5ca2bdf97fd4951084ea5d09f74388199
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-11-16 17:58:30 -0800

    Fix  false positives in orphanremover

--- src/components/OrphanRemover/index.js
+++ src/components/OrphanRemover/index.js
@@ -2,7 +2,7 @@ const replaceAt = (string, index, replacement) =>
   string.substr(0, index) + replacement + string.substr(index + 1)
 
 const OrphanRemover = ({ children }) => {
-  if (typeof children === 'string') {
+  if (typeof children === 'string' && children.includes(' ')) {
     const index = children.lastIndexOf(' ')
     const nbsp = '\xa0'
     return replaceAt(children, index, nbsp)
