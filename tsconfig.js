3e0ce0b8b21818a82f5642ac4affa21cd7c728d4
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-01 23:12:22 -0800

    Converted SectionHead to typescript

---
+++ tsconfig.js
@@ -0,0 +1,25 @@
+module.exports = {
+  compilerOptions: {
+    baseUrl: 'src',
+    target: 'es5',
+    lib: ['dom', 'dom.iterable', 'esnext'],
+    checkJs: false,
+    // allowJs: true,
+
+    // skipLibCheck: true,
+    // esModuleInterop: true,
+    // allowSyntheticDefaultImports: true,
+    // strict: true,
+
+    // forceConsistentCasingInFileNames: true,
+    // module: 'esnext',
+    // moduleResolution: 'node',
+    // resolveJsonModule: true,
+    // isolatedModules: true,
+    // noEmit: true,
+    // jsx: 'preserve',
+    // noImplicitAny: false,
+  },
+  include: ['src'],
+  exclude: ['node_modules'],
+}
