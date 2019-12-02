06dd36489dd6a27e3a9e83ab29e15853224c5bc9
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-01 16:44:11 -0800

    Redirect uri

--- src/pages/api/getSpotifyPlaylist.js
+++ src/pages/api/getSpotifyPlaylist.js
@@ -6,6 +6,7 @@ const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env
 const spotifyApi = new SpotifyWebApi({
   clientId: SPOTIFY_CLIENT_ID,
   clientSecret: SPOTIFY_CLIENT_SECRET,
+  redirectUri: 'http://localhost:9000/getSpotifyAuth',
 })
 
 async function getAccessToken() {
