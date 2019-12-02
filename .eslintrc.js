3408d6b6ae3b1a7d1af3e734b75b59fbe7ea823d
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-01 22:50:32 -0800

    More typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,22 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier'],
-  plugins: ['import', 'react', 'react-hooks'],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
+    '@tylermcrobert/eslint-config/react',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    'no-console': [1, { allow: ['warn', 'error'] }],
-    'no-underscore-dangle': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'react/forbid-prop-types': 0,
-    'import/no-cycle': 0,
     'react/prop-types': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'jsx-a11y/iframe-has-title': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'react/no-danger': 0,
-    'react/no-array-index-key': 0,
-    'react/react-in-jsx-scope': 0,
-    'import/prefer-default-export': 0,
-  },
-  env: {
-    browser: true,
-    node: true,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'import/extensions': 0,
   },
   settings: {
     'import/resolver': {
       node: {
+        paths: ['src'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        paths: ['/', 'src', __dirname],
-        moduleDirectory: ['node_modules'],
       },
     },
   },
