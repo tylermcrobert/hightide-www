08b791db80a13b9f15e71ff75cfe3e894a6d2837
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-01 16:55:50 -0800

    put spotifywebapi inside default export

--- src/pages/api/getSpotifyPlaylist.js
+++ src/pages/api/getSpotifyPlaylist.js
@@ -3,25 +3,18 @@ const SpotifyWebApi = require('spotify-web-api-node')
 
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: SPOTIFY_CLIENT_ID,
-  clientSecret: SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'http://localhost:9000/getSpotifyAuth',
-})
-
-async function getAccessToken() {
-  const res = await spotifyApi.clientCredentialsGrant()
-  return res.body.access_token
-}
-
 export default async (req, res) => {
   const { id } = req.query
 
   if (id) {
-    const token = await getAccessToken()
-    spotifyApi.setAccessToken(token)
+    const spotifyApi = new SpotifyWebApi({
+      clientId: SPOTIFY_CLIENT_ID,
+      clientSecret: SPOTIFY_CLIENT_SECRET,
+      redirectUri: 'http://localhost:9000/getSpotifyAuth',
+    })
 
-    console.log({ token })
+    const token = spotifyApi.clientCredentialsGrant()
+    spotifyApi.setAccessToken(token)
 
     try {
       const playlist = await spotifyApi.getPlaylist(id)
