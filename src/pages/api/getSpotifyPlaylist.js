fc291580989710579e70700130d761e06e9db1f6
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-01 16:48:29 -0800

    getspotifyplaylist error code

--- src/pages/api/getSpotifyPlaylist.js
+++ src/pages/api/getSpotifyPlaylist.js
@@ -21,11 +21,13 @@ export default async (req, res) => {
     const token = await getAccessToken()
     spotifyApi.setAccessToken(token)
 
+    console.log({ token })
+
     try {
       const playlist = await spotifyApi.getPlaylist(id)
       res.status(200).json(playlist)
-    } catch ({ ...error }) {
-      res.status(error.statusCode).json({ ...error })
+    } catch (error) {
+      res.status(400).json(error)
     }
   } else {
     res.status(401).json({ error: 'ID field is required' })
