df3b3068e81c93f2a37b981ea2351d6a91f5d916
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-02 18:30:08 -0800

    Fix lint errors

--- src/components/Arrow/index.tsx
+++ src/components/Arrow/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ArrowSvg = ({ ...props }) => (
+const ArrowSvg: React.FC = ({ ...props }) => (
   <Arrow {...props} viewBox="0 0 60 18" xmlns="http://www.w3.org/2000/svg">
     <g stroke="#000" strokeLinecap="square">
       <path d="M1.25 9H58.5M59 9l-9 9M59 9l-9-9" strokeWidth="1.4" />
