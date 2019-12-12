2171075b002b933362d40d803632b503a4723765
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-11 21:39:25 -0800

    Made getLayout make sense

--- src/templates/Journal/getLayout.tsx
+++ src/templates/Journal/getLayout.tsx
@@ -1,25 +1,33 @@
 /*
 
-a b
-a c
-d f
-e f
+Natural indexing
 
+0 1
+0 2
+3 4
+5 4
+
+...etc
+
+Intended indexing
+
+0 1
+0 2
+3 5
+4 5
 */
 const isNth = (i: number, nth: number): boolean => (i - nth) % 6 === 0
 
+// get presentational order
 const getOrder = (i: number): number => {
+  // swap 4 and 5
   if (isNth(i, 4)) return i + 1
   if (isNth(i, 5)) return i - 1
+  // otherwise return index
   return i
 }
 
-const getLayout = (
-  i: number
-): {
-  order: number
-  isLarge: boolean
-} => ({
+const getLayout = (i: number): { order: number; isLarge: boolean } => ({
   order: getOrder(i),
   isLarge: isNth(i, 0) || isNth(i, 5),
 })
