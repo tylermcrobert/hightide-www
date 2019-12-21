82db9e3515d2b2ba5065da4a91c0c20d556def9a
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2019-12-20 18:10:52 -0800

    Scroll off trigger causes it to cancel

--- src/components/CursorHover/CursorTrigger.tsx
+++ src/components/CursorHover/CursorTrigger.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
 import styled from 'styled-components'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import Router from 'next/router'
 import { useSetCursor } from './index'
 import { CursorType } from './types'
@@ -15,23 +15,61 @@ export const CursorTrigger: React.FC<Props> = ({
   ...props
 }) => {
   const { setCursor, currentCursor } = useSetCursor()
+  // to narrow down multiple DOM elements
+  const isActiveTrigger = useRef(false)
+  const DOMRef = useRef<HTMLDivElement>(null)
+  const mousePos = useRef<{ x: number; y: number }>({ x: 0, y: 0 })
 
-  const handleEnter = () => setCursor(cursor)
-  const handleLeave = () => setCursor(null)
+  const handleEnter = () => {
+    isActiveTrigger.current = true
+    setCursor(cursor)
+  }
+
+  const handleLeave = () => {
+    isActiveTrigger.current = false
+    setCursor(null)
+  }
+
+  const handleMouse = e => {
+    // record mouse position
+    if (isActiveTrigger.current) {
+      mousePos.current = { x: e.clientX, y: e.clientY }
+    }
+  }
+
+  const handleScroll = () => {
+    // if is current active trigger
+    if (isActiveTrigger.current && DOMRef.current) {
+      const b = DOMRef.current.getBoundingClientRect()
+
+      const isAbove = b.top > mousePos.current.y
+      const isBelow = b.bottom < mousePos.current.y
+      const isLeftOfEl = b.left > mousePos.current.x
+      const isRightofEl = b.right < mousePos.current.x
+      const isOutsideTrigger = isAbove || isBelow || isLeftOfEl || isRightofEl
+
+      if (isOutsideTrigger) {
+        handleLeave()
+      }
+    }
+  }
 
   // if cursor is triggered by lin
   Router.events.on('routeChangeComplete', handleLeave)
 
-  // useEffect(() => {
-  //   document.addEventListener('scroll', handleLeave)
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll)
+    document.addEventListener('mousemove', handleMouse)
 
-  //   return () => {
-  //     document.removeEventListener('scroll', handleLeave)
-  //   }
-  // }, [])
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+      document.addEventListener('mousemove', handleMouse)
+    }
+  }, [])
 
   return (
     <Trigger
+      ref={DOMRef}
       cursorVisible={!currentCursor}
       onMouseOver={handleEnter}
       onMouseLeave={handleLeave}
